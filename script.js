3c314333a32b0c9535b9fda52f9639e13f5b13e7
ayoub <ayoubzouiine@gmail.com>
2023-07-11 03:12:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function changecolor(){
+    document.body.style.backgroundColor='rgb('+Math.round(Math.random()*255)+
+    ','+Math.round(Math.random()*255)+
+    ','+Math.round(Math.random()*255)+')';
+}
+function chc(){
+    document.body.style.backgroundColor='rgb('+Math.round(Math.random()*255)+
+    ','+Math.round(Math.random()*255)+
+    ','+Math.round(Math.random()*255)+')';
+}
